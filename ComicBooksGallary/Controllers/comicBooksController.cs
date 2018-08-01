808c43066b26ef06d9d7a790e182022396460c34
ak <raiashutech@gmail.com>
2018-07-31 23:42:42 -0500

    DRY used

--- ComicBooksGallary/Controllers/comicBooksController.cs
+++ ComicBooksGallary/Controllers/comicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBooksGallary.Models;
+﻿using ComicBooksGallary.Data;
+using ComicBooksGallary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,19 @@ namespace ComicBooksGallary.Controllers
 {
     public class ComicBooksController : Controller
     {
+        private ComicBookRepository _comicBookRepository = null;
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
 
-            public ActionResult Detail()
+            public ActionResult Detail(int? id)
             {
-            var comicBook = new ComicBook()
+            if (id==null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                   new Artist(){Name="Dan Slott", Role="Script"},
-                   new Artist(){Name="Humberto Ramos", Role="Pencils"},
-                   new Artist(){Name="Victor Olazaba", Role="Inks"},
-                   new Artist(){Name="Edgar Delgado", Role="Colors"},
-                   new Artist(){Name="Chris Eliopoulos", Role="Letters"},
-                }
-            };
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);             
             }
     }
